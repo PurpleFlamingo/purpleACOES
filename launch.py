aecaf2a83eaa6c3a475aa8728960726393b17b06
Cristian Rafael Ávila García <36643196+avilavt@users.noreply.github.com>
2018-11-25 20:47:31 +0100

    Corrección de unos pequeños errores

---
+++ launch.py
@@ -0,0 +1,83 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget
+from gestorUsuario import GestorUsuario
+from recovery import Recovery
+from warning import Advertencia
+from BD.BD import BD
+
+
+form_1, base_1 = uic.loadUiType('UI/login.ui')
+
+class LogIn(base_1, form_1):
+    def __init__(self):
+        super(base_1,self).__init__()
+        self.setupUi(self)
+        self.child = None
+
+        #variable para llevar la cuenta de los intentos erroneos consecutivos que se producen al iniciar sesión
+        self.numeroIntentosErroneos = 0 
+        self.eUser.setFocus(True)
+        self.bRecordatorio.clicked.connect(self.recordatorio)
+        self.bLogin.clicked.connect(self.usuario)
+        self.bExit.clicked.connect(self.salir)
+
+    
+    def usuario(self):
+        #si el usuario no introduce el nombre de usuario o la contraseña el botón no hace nada
+        if (self.eUser.text() != '') and (self.ePassword.text()) != '':
+            if(self.inicio_sesion(self.eUser.text(), self.ePassword.text())):
+                #Al iniciarse correctamente la sesión el número de intentos erroneos se resetea y se borra el mensaje de error
+                self.numeroIntentosErroneos = 0
+                self.lWarning.setText("")
+                self.eUser.setText("")
+                self.ePassword.setText("")
+                if self.child is None or self.child != GestorUsuario(self):
+                    self.child = GestorUsuario(self)
+                    self.child.show()
+                    self.hide()
+                else:
+                    self.hide()
+                    self.child.show()
+            else:
+                #si el usuario introduce el nombre de usuario o contraseña incorrectos la
+                #aplicación muestra un mensaje en la pantalla de inicio de sesión, si lo 
+                #hace por quinta vez consecutiva se muestra una ventana de dialogo comunicando
+                #este hecho que solo tiene un botón que cierra la aplicación. 
+                self.numeroIntentosErroneos += 1
+                if(self.numeroIntentosErroneos < 5):
+                    self.lWarning.setText("Usuario o contraeña incorrectos")
+                else:
+                    self.child = Advertencia(self)
+                    self.child.setModal(True)
+                    self.child.show()
+                    
+                
+            
+    def salir(self):
+        exit()
+
+    def recordatorio(self):
+        if self.child is None or self.child != Recovery(self):
+            self.child = Recovery(self)
+            self.child.show()
+            self.hide()
+        else:
+            self.hide()
+            self.child.show()
+
+    #método que evalua si el nombre de usuario y la contraseña estan en la base de datos y son correctos.
+    def inicio_sesion(self,user: str, password: str):
+        self.db = BD()
+        self.nombre = "LOWER(`nombre`) = \"" + user.lower() + "\""
+        self.claveBD = self.db.selectEscalar("clave","usuario",self.nombre)
+        if self.claveBD == None or password != self.claveBD[0]:
+            return False
+        else:
+            return True
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    loginW = LogIn()
+    loginW.show()
+    sys.exit(app.exec_())
---
+++ recovery.py
@@ -0,0 +1,65 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget
+from BD.BD import BD
+from correoElectronico import CorreoElectronico
+from passwordDisplay import PasswordDisplay
+
+form_1, base_1 = uic.loadUiType('UI/recovery.ui')
+
+class Recovery(base_1, form_1):
+    def __init__(self, parent = None):
+        super(base_1,self).__init__()
+        self.setupUi(self)
+        self.parent = parent
+        self.child = None
+        self.claveRecuperada = None
+        
+        self.eUserName.setFocus()
+        self.bExit.clicked.connect(self.salir)
+        self.bOk.clicked.connect(self.olvido)
+
+    def salir(self):
+        self.hide()
+        self.parent.show()
+
+    def olvido(self):
+        nombre = self.eUserName.text().replace(' ','')
+        self.recuperatorio(nombre)
+
+
+    def recuperatorio(self,user: str):
+        if user != None:
+            if user != "":
+                self.db = BD()
+                self.nombre = "LOWER(`nombre`) = \"" + user.lower() + "\""
+                self.claveBD = self.db.selectEscalar("id_usuario, clave, rol","usuario",self.nombre)
+                if self.claveBD != None:
+                    self.claveRecuperada = self.claveBD[1]
+                    if self.claveBD[2].lower == "socio":
+                        self.tabla = "socio"
+                    else:
+                        self.tabla = "voluntario"
+                    self.cond = "LOWER(`usuario`) = " + (str(self.claveBD[0])).lower()
+
+                    self.emailDB = self.db.selectEscalar("correo_electronico",self.tabla,self.cond)
+                    if(self.emailDB == None):
+                        if self.child is None or self.child != CorreoElectronico(self):
+                            self.child = CorreoElectronico(self)
+                            self.child.setModal(True)
+                            self.child.show()
+                    else:
+                        print("La contraseña es :", self.claveRecuperada)
+                        if self.child is None or self.child != PasswordDisplay(self):
+                            self.child = PasswordDisplay(self)
+                            self.child.setModal(True)
+                            self.child.show()
+
+                
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    recoveryW = Recovery()
+    recoveryW.show()
+    sys.exit(app.exec_())

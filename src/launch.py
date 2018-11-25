a98c87c62fb4203c7cf2898e9d8d7d3108bf0cd7
Cristian Rafael Ávila García <36643196+avilavt@users.noreply.github.com>
2018-11-25 13:56:21 +0100

    Implementación Inicio de sesión
    El funcionamiento del botón login es:
         * si el nombre de usuario o la contraseña están vacíos, no hace nada
         * en caso contrario paso al método inicio_sesión el nombre de usario y la contraseña
         * inicio_sesión se conecta con la base de datos y  solicita en la tabla usuario la contraseña de ese usuario
         * si el usuario no existe (se recibe una tupla vacia de la consulta) o la contraseña que se introdujo es erronea el método devuelve falso
         * si el usuario existe y la contraseña es correcta el método devuelve verdadero
         * al recibir la respuesta del método, si esta es verdadera, se reinicia el conteo de intentos erroneos y se inicia la aplicacion
         * si se recibe una respuesta falsa, se incrementa en uno el conteo de intentos erroneos y si es menor de 5 se muestra un mensaje
         * y si ese conteo es 5 se muestra un cuadro de dialogo, que bloqua la ventana de inicio de sesión y que solo tiene un botón que 
    cierra la aplicacion.

--- src/launch.py
+++ src/launch.py
@@ -3,6 +3,8 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
 from gestorUsuario import GestorUsuario
 from recovery import Recovery
+from warning import Advertencia
+from BD.BD import BD
 
 
 form_1, base_1 = uic.loadUiType('UI/login.ui')
@@ -13,16 +15,41 @@ class LogIn(base_1, form_1):
         self.setupUi(self)
         self.child = None
 
+        
+
+        #variable para llevar la cuenta de los intentos erroneos consecutivos que se producen al iniciar sesión
+        self.numeroIntentosErroneos = 0 
+
         self.bRecordatorio.clicked.connect(self.recordatorio)
         self.bLogin.clicked.connect(self.usuario)
         self.bExit.clicked.connect(self.salir)
 
-    #TODO: Esta funcion debe ser modificada para que redireccione a la ventana de seleccion de subsistema
+    
     def usuario(self):
-        self.child = GestorUsuario(self)
-        self.child.show()
-        self.hide()
-
+        #si el usuario no introduce el nombre de usuario o la contraseña el botón no hace nada
+        if (self.eUser.text() != '') and (self.ePassword.text()) != '':
+            if(self.inicio_sesion(self.eUser.text(), self.ePassword.text())):
+                #Al iniciarse correctamente la sesión el número de intentos erroneos se resetea
+                self.numeroIntentosErroneos = 0
+                if self.child is None:
+                    self.child = GestorUsuario(self)
+                    self.child.show()
+                    self.hide()
+            else:
+                #si el usuario introduce el nombre de usuario o contraseña incorrectos la
+                #aplicación muestra un mensaje en la pantalla de inicio de sesión, si lo 
+                #hace por quinta vez consecutiva se muestra una ventana de dialogo comunicando
+                #este hecho que solo tiene un botón que cierra la aplicación. 
+                self.numeroIntentosErroneos += 1
+                if(self.numeroIntentosErroneos < 5):
+                    self.lWarning.setText("Usuario o contraeña incorrectos")
+                else:
+                    self.child = Advertencia(self)
+                    self.child.setModal(True)
+                    self.child.show()
+                    
+                
+            
     def salir(self):
         exit()
 
@@ -31,6 +58,15 @@ class LogIn(base_1, form_1):
         self.child.show()
         self.hide()
 
+    #método que evalua si el nombre de usuario y la contraseña estan en la base de datos y son correctos.
+    def inicio_sesion(self,user: str, password: str):
+        self.db = BD()
+        self.nombre = "nombre = \"" + user + "\""
+        self.claveBD = self.db.selectEscalar("clave","usuario",self.nombre)
+        if self.claveBD == None or password != self.claveBD[0]:
+            return False
+        else:
+            return True
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
---
+++ src/warning.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget
+
+form_1, base_1 = uic.loadUiType('UI/warning.ui')
+
+class Advertencia(base_1, form_1):
+    def __init__(self, parent = None):
+        super(base_1,self).__init__()
+        self.setupUi(self)
+        self.parent = parent
+        self.child = None
+
+        self.bAceptar.clicked.connect(parent.salir)

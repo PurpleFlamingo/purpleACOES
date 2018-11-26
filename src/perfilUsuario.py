339def02cc89e3f0fe46b5324c92566c72dc60ff
MiRA0001 <42837318+MiRA0001@users.noreply.github.com>
2018-11-26 15:19:19 +0100

    perfilUsuario.py añadido

---
+++ src/perfilUsuario.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget
+from BD import BD
+
+form_1, base_1 = uic.loadUiType('UI/perfilUsuario.ui')
+
+class PerfilUsuario(base_1, form_1):
+    def __init__(self, parent = None):
+        super(base_1,self).__init__()
+        self.setupUi(self)
+        self.parent = parent
+        self.child = []
+        self.bAtras.clicked.connect(self.atras)
+
+    def atras(self):
+        self.parent.show()
+        self.hide()
+    
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    perfilUsuarioW = PerfilUsuario()
+    perfilUsuarioW.show()
+    sys.exit(app.exec_())

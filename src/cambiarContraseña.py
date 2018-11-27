ec19d580e89f3b092bf4cf6f879a2f9350b645f6
Sera <shafledome@gmail.com>
2018-11-28 00:47:51 +0100

    cambios en cambiarContraseña.py

--- src/cambiarContraseña.py
+++ src/cambiarContraseña.py
@@ -13,38 +13,43 @@ class CambiarContrasenia(base_1, form_1):
         self.setupUi(self)
         self.bAtras.clicked.connect(self.atras)
         self.bEnviar.clicked.connect(self.enviardatos)
-        bd = BDOperaciones()
-        print("durum")
-        print(bd.getUsuario(self.idUsuario)[2])
-        self.lMensaje.setText(bd.getUsuario(self.idUsuario)[2])
+        print(self.idUsuario)
+
 
     # Metodo que devuelve a perfil de usuario pulsando el boton atras
     def atras(self):
-        self.parent.show()
-        self.close()
+        bd = BDOperaciones()
+        contrasenia = str(bd.getUsuario(self.idUsuario)[2])
+        print(self.idUsuario)
+        print(contrasenia + " kebab")
+        print(self.eCAntigua.text())
+        print(self.eCNueva.text())
+        print(self.eCConfirmacion.text())
+        #self.parent.show()
+        #self.close()
 
     # Metodo que actualiza la contraseña de usuario o notifica un error en la bd
-    def enviardatos(self):
+    def enviardatos(self, idusuario: int):
         bd = BDOperaciones()
-        contrasenia = bd.getUsuario(idUsuario)[2]
-        self.lMensaje.setText(contrasenia)
-        if str(contrasenia) != str(self.eCAntigua.text()):
-            self.lMensaje.setText("Contraseña actual incorrecta.")
-            self.eCAntigua.setText("")
-            self.eCNueva.setText("")
-            self.eCConfirmacion.setText("")
-        if str(self.eCNueva.text()) != str(self.eCConfirmacion.text()):
-            self.lMensaje.setText("Confirmación de contraseña es distinta a la nueva contraseña.")
-            self.eCAntigua.setText("")
-            self.eCNueva.setText("")
-            self.eCConfirmacion.setText("")
-        else:
-            bd.setPassUsuario(eCNueva.text(), iduser)
-            self.lMensaje.setText("Se ha cambiado la contraseña con exito.")
+        contrasenia = bd.getUsuario(self.idUsuario)[2]
+        if (self.eCNueva.text() != "") and (self.eCConfirmacion.text() != ""):
+            if contrasenia != self.eCAntigua.text():
+                self.lMensaje.setText("Contraseña actual incorrecta.")
+                self.eCAntigua.setText("")
+                self.eCNueva.setText("")
+                self.eCConfirmacion.setText("")
+            elif self.eCNueva.text() != self.eCConfirmacion.text():
+                self.lMensaje.setText("Confirmación de contraseña es distinta a la nueva contraseña.")
+                self.eCAntigua.setText("")
+                self.eCNueva.setText("")
+                self.eCConfirmacion.setText("")
+            else:
+                bd.setPassUsuario(str(self.eCNueva.text()), idusuario)
+                self.lMensaje.setText("Se ha cambiado la contraseña con exito.")
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    cambiarContraseniaW = CambiarContrasenia(1)
+    cambiarContraseniaW = CambiarContrasenia(2)
     cambiarContraseniaW.show()
     sys.exit(app.exec_())

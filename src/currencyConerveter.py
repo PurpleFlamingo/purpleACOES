7f57033cd0dc284f87dac6660b5c6054bee16328
jfaldanam <jfaldanam@gmail.com>
2018-12-12 12:54:12 +0100

    Created class for currency conversions

--- src/currencyConerveter.py
+++ src/currencyConerveter.py
@@ -1,7 +1,23 @@
 import requests
-query = 'https://free.currencyconverterapi.com/api/v6/convert?q=EUR_HNL,HNL_EUR&compact=ultra'
+from datetime import datetime
 
-result = requests.get(query)
 
-print('Status:{}'.format(result.status_code))
-print(result.json())
+
+
+class CurrencyConverter:
+    def __init__(self):
+        currentRatio = self.getCurrentRatio()
+
+        self.EUR_HNL = currentRatio
+        self.HNL_EUR = currentRatio
+
+    def getCurrentRatio(self):
+        query = 'https://free.currencyconverterapi.com/api/v6/convert?q=EUR_HNL,HNL_EUR&compact=ultra'
+        result = requests.get(query)
+
+        status = result.status_code
+        return result.json()
+
+if __name__ == '__main__':
+    cur = CurrencyConverter()
+    print(cur.EUR_HNL)

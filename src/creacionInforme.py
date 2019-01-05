115c5b0234181c835bff8feb42d3d17af39f823d
jfaldanam <jfaldanam@gmail.com>
2019-01-05 18:06:24 +0100

    Using easygui to ask for a folder to save report

--- src/creacionInforme.py
+++ src/creacionInforme.py
@@ -6,6 +6,7 @@ from BD.BDOperaciones import BDOperaciones
 from BD.Transaccion import Transaccion
 from BD.Proyecto import Proyecto
 from BD.Apadrinamiento import Apadrinamiento
+import easygui
 
 
 form_1, base_1 = uic.loadUiType('UI/creacionInforme.ui')
@@ -31,6 +32,9 @@ class CreacionInforme(base_1, form_1):
         self.close()   
 
     def generar(self):
+        folderName = easygui.diropenbox()
+        if folderName == None:
+            return
         transacciones = Transaccion.listaTransacciones()
         tipo = self.cTipo.currentText()
         tipoBool = tipo == 'Gasto'
@@ -50,21 +54,26 @@ class CreacionInforme(base_1, form_1):
 
         db = BDOperaciones()
         header = db.nombreColumnas(Transaccion.tabla)
-        with open('/home/jfaldanam/Software/purpleACOES/src/DATA/{}.csv'.format(datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')), 'w') as file:
-            for i, col in enumerate(header):
-                if i < len(header)-1:
-                    file.write(col + ',')
-                else:
-                    file.write(col + '\n')
-            for trans in result:
-                file.write(str(trans.getIdTransaccion()) + ',')
-                file.write('True' if trans.getGasto() else 'False' + ',')
-                file.write(trans.getFechaEmision().strftime('%Y-%m-%d') + ',')
-                file.write(str(trans.getCuantia()) + ',')
-                file.write(trans.getMoneda() + ',')
-                file.write(trans.getDestino() if trans.getDestino()!=None else '' + ',')
-                file.write(trans.getFormaPago() if trans.getFormaPago()!=None else '' + ',')
-                file.write(trans.getMotivo() if trans.getMotivo()!=None else '' + ',')
-                file.write(str(trans.getProyecto().getIdProyecto() if trans.getProyecto()!=None else '') + ',')
-                file.write(str(trans.getApadrinamiento().getIdApadrinamiento() if trans.getApadrinamiento()!= None else '') + ',')
-                file.write((trans.getBeneficiario() if trans.getBeneficiario()!=None else '') + '\n')
+        try:
+            with open('{}/{}.csv'.format(folderName, datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')), 'w') as file:
+                for i, col in enumerate(header):
+                    if i < len(header)-1:
+                        file.write(col + ',')
+                    else:
+                        file.write(col + '\n')
+                for trans in result:
+                    file.write(str(trans.getIdTransaccion()) + ',')
+                    file.write('True' if trans.getGasto() else 'False' + ',')
+                    file.write(trans.getFechaEmision().strftime('%Y-%m-%d') + ',')
+                    file.write(str(trans.getCuantia()) + ',')
+                    file.write(trans.getMoneda() + ',')
+                    file.write(trans.getDestino() if trans.getDestino()!=None else '' + ',')
+                    file.write(trans.getFormaPago() if trans.getFormaPago()!=None else '' + ',')
+                    file.write(trans.getMotivo() if trans.getMotivo()!=None else '' + ',')
+                    file.write(str(trans.getProyecto().getIdProyecto() if trans.getProyecto()!=None else '') + ',')
+                    file.write(str(trans.getApadrinamiento().getIdApadrinamiento() if trans.getApadrinamiento()!= None else '') + ',')
+                    file.write((trans.getBeneficiario() if trans.getBeneficiario()!=None else '') + '\n')
+            #Close the UI
+            self.close()
+        except FileNotFoundError:
+            print("Folder not found to save the report")
